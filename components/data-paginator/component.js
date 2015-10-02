503ee6c6a9aa0f8f6fe43e4cb55bc48ff75010d6
guigouz <guigouz@gmail.com>
2015-10-02 00:54:10 -0400

    fixes pagination

--- components/data-paginator/component.js
+++ components/data-paginator/component.js
@@ -15,106 +15,37 @@ define(['text!./view.html'], function (template) {
         //    params.query.filter.set('_limit', undefined === params.limit ? 50 : params.limit );
         //}
 
-        self.count = query.observable({_fields: 'COUNT(*)'}, function (result) {
-            return parseInt(result[0]['COUNT(*)'], 10);
-        });
+        self.count = query.totalElements;
+        
         //filter: jQuery.extend({ default_field: 'q', fields: null }, params.filter || {}),
-        self.page = ko.observable(1);
-        self.rows_per_page = ko.computed({
-            read: function() {
-                return query.params()._limit;
-            },
-            write: function(limit) {
-                var params = query.params();
-
-                if(params._limit !== limit) {
-                    params._limit = limit;
-                    query.params.valueHasMutated();
-                }
-
-            }
+        self.page = ko.computed(function() {
+        	return query.number() + 1;
         });
+        
+        self.rows_per_page = query.size;
 
         self.page.next = function () {
             if(self.page() < self.pages())
-                self.page(self.page() + 1);
+                query.params.page(self.page());
         };
+        
         self.page.prev = function () {
             if(self.page() > 1)
-                self.page(self.page() - 1);
+                query.params.page(query.number() - 1);
         };
 
         self.page.first = function() {
             if(self.page() > 1)
-                self.page(1);
+                query.params.page(0);
         };
 
         self.page.last = function () {
             if(self.page() < self.pages())
-                self.page(self.pages());
+                query.params.page(self.pages() - 1);
         };
 
-        //
-        self.offset = ko.computed(function () {
-            var params = query.params(),
-                offset = (self.page() - 1) * self.rows_per_page();
 
-            // bind to filter updates also
-            query.filter();
-
-            if(params._offset !== offset) {
-                params._offset = offset;
-                query.params.valueHasMutated();
-            }
-
-            return parseInt(offset, 10);
-        }, self); //.extend({ throttle: 500 });
-
-        //
-        //
-        self.pages = ko.computed(function () {
-            return Math.ceil(this.count() / this.rows_per_page());
-        }, self);
-
-
-
-
-        //
-        //self.paginator.query.parse = function(str) {
-        //    var re = /([^ :]+):?([^ ]*)/g,
-        //        q = null,
-        //        query = {};
-        //
-        //    while(str !== undefined && (q = re.exec(str)) !== null) {
-        //        if(q[2].length > 0) {
-        //            query[q[1]] = q[2]; // TODO check if query[q[1]] already exists
-        //        }
-        //        else {
-        //            // TODO default_field may be an array [ 'OR', 'field1', 'field2' ] or object ?
-        //            query[self.paginator.filter.default_field] = q[1] + '%';
-        //        }
-        //    }
-        //
-        //    return query;
-        //};
-        //
-        //self.paginator.query.subscribe(function (newValue) {
-        //
-        //    var page = self.paginator.page(),
-        //        query = self.paginator.query.parse(newValue);
-        //
-        //    nav.filter.reset(query);
-        //
-        //    if (page !== 1) {
-        //        self.paginator.page(1);
-        //    }
-        //    else {
-        //        self.paginator.page.valueHasMutated();
-        //    }
-        //
-        //    return query;
-        //});
-        //
+        self.pages = query.totalPages;
 
     };
 
